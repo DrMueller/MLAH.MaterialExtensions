1ad5eaf1652b1711f1ab5c52165bf1af568f4aba
Matthias Mueller <matthiasm@live.de>
2018-11-13 21:42:19 +0100

    WIP

--- projects/drmueller/ng-material-extensions/src/public_api.ts
+++ projects/drmueller/ng-material-extensions/src/public_api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ng-mat-extensions
  */
 
- export * from './lib/areas/modals/index';
- export * from './lib/areas/tables/index';
- export * from './lib/areas/ng-material-extensions.module';
- 
+export * from './lib/areas/modals/index';
+export * from './lib/areas/tables/index';
+export * from './lib/areas/ng-material-extensions.module';
