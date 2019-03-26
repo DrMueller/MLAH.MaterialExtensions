bffc77be099e41bd66241fe06b11ba9ffc31300c
Matthias Mueller <matthiasm@live.de>
2019-03-26 07:31:06 +0100

    WIP

--- projects/drmueller/ng-material-extensions/src/lib/areas/tables/components/mat-table/mat-table.component.ts
+++ projects/drmueller/ng-material-extensions/src/lib/areas/tables/components/mat-table/mat-table.component.ts
@@ -13,18 +13,36 @@ import { ColumnDefinitionsContainer, TableRowSelectionType } from '../../models/
   styleUrls: ['./mat-table.component.scss']
 })
 export class MatTableComponent<T> implements OnInit, AfterViewInit {
-  @Input() public set data(values: T[]) {
-    this._data = values;
-    if (this.matTable) {
-      this.bindData();
-    }
-  }
-
+  @Input() public columnDefinitions: ColumnDefinitionsContainer;
   @Input() public idSelector: keyof T;
+  @Output() public selectionChanged = new EventEmitter<T[]>();
+
+  public searchText: string;
+  public selection: SelectionModel<T>;
+
+  @ViewChild(MatPaginator) public matPaginator: MatPaginator;
+  @ViewChild(MatSort) public matSort: MatSort;
+  @ViewChild(MatTable) public matTable: MatTable<T>;
+
+  private _dataSource: MatTableDataSource<T>;
+  private _rowSelectionType: TableRowSelectionType;
+  private _data: T[];
 
   @Input() public set rowSelectionType(value: TableRowSelectionType) {
     this._rowSelectionType = value;
     this.initializeDataSource();
+  }
+
+  public deleteEntries(entries: T[]): void {
+    entries.forEach(entry => {
+      const dtoIndex = this._data.indexOf(entry);
+      this._data.splice(dtoIndex, 1);
+    });
+
+    this.selection.deselect(...entries);
+    this.bindData();
+    this.selectionChanged.emit(this.selection.selected);
+    this.matTable.renderRows();
   }
 
   public get allColumnHeaders(): string[] {
@@ -42,37 +60,6 @@ export class MatTableComponent<T> implements OnInit, AfterViewInit {
     return this._rowSelectionType !== TableRowSelectionType.ReadOnly;
   }
 
-  @Input() public columnDefinitions: ColumnDefinitionsContainer;
-
-  @Output() public selectionChanged = new EventEmitter<T[]>();
-
-  public searchText: string;
-  public get dataSource(): MatTableDataSource<T> {
-    return this._dataSource;
-  }
-
-  public selection: SelectionModel<T>;
-  @ViewChild(MatPaginator) public matPaginator: MatPaginator;
-  @ViewChild(MatSort) public matSort: MatSort;
-  @ViewChild(MatTable) public matTable: MatTable<T>;
-
-  private _dataSource: MatTableDataSource<T>;
-
-  private _rowSelectionType: TableRowSelectionType;
-  private _data: T[];
-
-  public deleteEntries(entries: T[]): void {
-    entries.forEach(entry => {
-      const dtoIndex = this._data.indexOf(entry);
-      this._data.splice(dtoIndex, 1);
-    });
-
-    this.selection.deselect(...entries);
-    this.bindData();
-    this.selectionChanged.emit(this.selection.selected);
-    this.matTable.renderRows();
-  }
-
   public isRowSelected(row: T): boolean {
     return this.selection.isSelected(row);
   }
@@ -86,13 +73,22 @@ export class MatTableComponent<T> implements OnInit, AfterViewInit {
     this.initializeDataSource();
   }
 
+  public searchTextChanged(newSearchText: string): void {
+    this.dataSource.filter = newSearchText.toLocaleLowerCase();
+  }
+
   public toggleRowSelection(row: T): void {
     this.selection.toggle(row);
     this.selectionChanged.emit(this.selection.selected);
   }
-
-  public searchTextChanged(newSearchText: string): void {
-    this.dataSource.filter = newSearchText.toLocaleLowerCase();
+  @Input() public set data(values: T[]) {
+    this._data = values;
+    if (this.matTable) {
+      this.bindData();
+    }
+  }
+  public get dataSource(): MatTableDataSource<T> {
+    return this._dataSource;
   }
 
   private bindData(): void {
