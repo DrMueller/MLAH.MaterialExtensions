238c3ba2d2bb320acd50258b4c05bac5f1a76ece
Matthias Müller <MatthiasM@Live.de>
2019-06-28 22:38:41 +0200

    WIP

--- projects/drmueller/ng-material-extensions/src/lib/areas/tables/components/button-template/button-template.component.spec.ts
+++ projects/drmueller/ng-material-extensions/src/lib/areas/tables/components/button-template/button-template.component.spec.ts
@@ -1,16 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonTemplateComponent } from './button-template.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('ButtonBaseTemplateComponent', () => {
+describe('ButtonTemplateComponent', () => {
   let component: ButtonTemplateComponent;
   let fixture: ComponentFixture<ButtonTemplateComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonTemplateComponent ]
+      declarations: [ButtonTemplateComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
